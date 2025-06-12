5b40cb1159c3b83f0a4db1bda5ea47d1f809469b
JaredDevAxity <jared.trejo@axity.com>
2025-06-12 15:53:11 -0600

    Starting JavaBank ATM project

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("No se puede dividir por cero!");
+        }
+    }
+}
